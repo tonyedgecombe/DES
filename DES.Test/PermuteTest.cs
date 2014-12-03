fbddb912372bb9ed1aea78815713b437a792b35e
Tony Edgecombe <tonyedgecombe@gmail.com>
2014-12-03 15:11:51 +0000

    Initial commit includes Permute function

---
+++ DES.Test/PermuteTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace DES.Test
+{
+    [TestFixture]
+    public class PermuteTest
+    {
+        [Test]
+        public void TestSimplePermute()
+        {
+            var permutation = Enumerable.Range(1, 64).ToList();
+
+            Assert.That(Program.Permute(0, 64, permutation), Is.EqualTo(0));
+
+            Assert.That(Program.Permute(0xFFFFFFFFFFFFFFFF, 64, permutation), Is.EqualTo(0xFFFFFFFFFFFFFFFF));
+        }
+
+        [Test]
+        public void TestPermute()
+        {
+            var permutation = new List<int> {4, 3, 2, 1};
+            Assert.That(Program.Permute(10, 4, permutation), Is.EqualTo(5));
+            Assert.That(Program.Permute(8, 4, permutation), Is.EqualTo(1));
+            Assert.That(Program.Permute(12, 4, permutation), Is.EqualTo(3));
+            Assert.That(Program.Permute(4, 4, permutation), Is.EqualTo(2));
+        }
+    }
+}
---
+++ DES/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DES
+{
+    public class Program
+    {
+        static void Main()
+        {
+        }
+
+        static public ulong Permute(ulong val, int size, List<int> changes)
+        {
+            Debug.Assert(size == changes.Count);
+
+            ulong result = 0;
+
+            for (int i = size - 1; val != 0; i--)
+            {
+                result |= (val & 1) << size - changes[i];
+                val >>= 1;
+            }
+
+            return result;
+        }
+    }
+}
