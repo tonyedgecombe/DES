33e0d4f9a1211f7e9b9b81ccffede5bb5a1d9c37
Tony Edgecombe <tonyedgecombe@gmail.com>
2014-12-04 17:15:27 +0000

    Key schedule now complete

--- DES.Test/PermuteTest.cs
+++ DES.Test/PermuteTest.cs
@@ -44,8 +44,31 @@ namespace DES.Test
         {
             Assert.That(DES.LeftShift(0x01000000000, 0x01), Is.EqualTo(0x02000000000));
             Assert.That(DES.LeftShift(0x01000000000, 0x02), Is.EqualTo(0x04000000000));
-            Assert.That(DES.LeftShift(0x7654321000000000, 0x04), Is.EqualTo(0x6543210000000000));
-            Assert.That(DES.LeftShift(0xF654321000000000, 0x04), Is.EqualTo(0x6543211000000000));
+            Assert.That(DES.LeftShift(0x7654321000000000, 0x04), Is.EqualTo(0x6543217000000000));
+            Assert.That(DES.LeftShift(0xF654321000000000, 0x04), Is.EqualTo(0x654321F000000000));
+            Assert.That(DES.LeftShift(0xFFFFFFF000000000, 0x01), Is.EqualTo(0xFFFFFFF000000000));
+            Assert.That(DES.LeftShift(0x8000000000000000, 0x01), Is.EqualTo(0x0000001000000000));
+        }
+
+        [Test]
+        public void TestConcat()
+        {
+            const ulong left = 0x1234567100000000;
+            const ulong right = 0x89ABCDE100000000;
+
+            Assert.That(DES.Concat(left, right), Is.EqualTo(0x123456789ABCDE00));
+        }
+
+        [Test]
+        public void TestKeySchedule()
+        {
+            var result = DES.KeySchedule(0x133457799BBCDFF1);
+
+            Assert.That(result[1], Is.EqualTo(0x1B02EFFC70720000));
+            Assert.That(result[2], Is.EqualTo(0x79AED9DBC9E50000));
+            Assert.That(result[3], Is.EqualTo(0x55FC8A42CF990000));
+            
+            Assert.That(result[16], Is.EqualTo(0xCB3D8B0E17F50000));
         }
     }
 }
--- DES/DES.cs
+++ DES/DES.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace DES
 {
     public static class DES
@@ -12,6 +15,18 @@ namespace DES
             7, 62, 54, 46, 38, 30, 22,
             14, 6, 61, 53, 45, 37, 29,
             21, 13, 5, 28, 20, 12, 4
+        };
+
+        public static int[] PC2 =
+        {
+            14, 17, 11, 24, 1, 5,
+            3, 28, 15, 6, 21, 10,
+            23, 19, 12, 4, 26, 8,
+            16, 7, 27, 20, 13, 2,
+            41, 52, 31, 37, 47, 55,
+            30, 40, 51, 45, 33, 48,
+            44, 49, 39, 56, 34, 53,
+            46, 42, 50, 36, 29, 32
         };
 
 
@@ -41,21 +56,59 @@ namespace DES
             return (val << 28) & 0xFFFFFFF000000000;
         }
 
+        // Join to 56 bit values
+        public static ulong Concat(ulong left, ulong right)
+        {
+            return (left & 0xFFFFFFF000000000) | ((right & 0xFFFFFFF000000000) >> 28);
+        }
+
         // 56 bit left shift
         public static ulong LeftShift(ulong val, int count)
         {
-            var msb = val & 0x8000000000000000;
-            return (val << count) & 0xFFFFFFF000000000 | msb >> 27;
+            for (int i = 0; i < count; i++)
+            {
+                var msb = val & 0x8000000000000000;
+                val = (val << 1) & 0xFFFFFFE000000000 | msb >> 27;
+            }
+
+            return val;
         }
 
         public static int[] LeftShifts = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
 
+        public static List<ulong> KeySchedule(ulong key)
+        {
+            var p = Permute(key, PC1);
+            var c = Left(p);
+            var d = Right(p);
 
+            var schedule = new List<Pair> {new Pair {Left = c, Right = d}};
+
+            for (int i = 1; i <= LeftShifts.Count(); i++)
+            {
+                schedule.Add(new Pair
+                {
+                    Left = LeftShift(schedule[i - 1].Left, LeftShifts[i - 1]),
+                    Right = LeftShift(schedule[i - 1].Right, LeftShifts[i - 1])
+                });
+            }
+
+            var result = new List<ulong>();
+
+            for (int i = 0; i < schedule.Count; i++)
+            {
+                var joined = Concat(schedule[i].Left, schedule[i].Right);
+                var permuted = Permute(joined, PC2);
+
+                result.Add(permuted);
+            }
+            return result;
+        }
     }
 
     public struct Pair
     {
-        public uint Left;
-        public uint Right;
+        public ulong Left;
+        public ulong Right;
     }
 }
