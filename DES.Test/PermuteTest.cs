6aea37115446257b9872a767c8132606d82e3685
Tony Edgecombe <tonyedgecombe@gmail.com>
2014-12-04 16:27:08 +0000

    ulong values all left aligned

--- DES.Test/PermuteTest.cs
+++ DES.Test/PermuteTest.cs
@@ -30,21 +30,22 @@ namespace DES.Test
         [Test]
         public void TestLeft()
         {
-            Assert.That(DES.Left(0x0123456789ABCDEF), Is.EqualTo(0x0123456));
+            Assert.That(DES.Left(0x0123456789ABCDEF), Is.EqualTo(0x0123456000000000));
         }
 
         [Test]
         public void TestRight()
         {
-            Assert.That(DES.Right(0x0123456789ABCDEF), Is.EqualTo(0x789ABCD));
+            Assert.That(DES.Right(0x0123456789ABCDEF), Is.EqualTo(0x789ABCD000000000));
         }
 
         [Test]
         public void TestLeftShift()
         {
-            Assert.That(DES.LeftShift(0x01, 0x01), Is.EqualTo(0x02));
-            Assert.That(DES.LeftShift(0x01, 0x02), Is.EqualTo(0x04));
-            Assert.That(DES.LeftShift(0x08000000, 0x01), Is.EqualTo(0x01));
+            Assert.That(DES.LeftShift(0x01000000000, 0x01), Is.EqualTo(0x02000000000));
+            Assert.That(DES.LeftShift(0x01000000000, 0x02), Is.EqualTo(0x04000000000));
+            Assert.That(DES.LeftShift(0x7654321000000000, 0x04), Is.EqualTo(0x6543210000000000));
+            Assert.That(DES.LeftShift(0xF654321000000000, 0x04), Is.EqualTo(0x6543211000000000));
         }
     }
 }
--- DES/DES.cs
+++ DES/DES.cs
@@ -4,21 +4,21 @@ namespace DES
     {
         public static int[] PC1 =
         {
-            57, 49, 41, 33, 25, 17,  9,
-             1, 58, 50, 42, 34, 26, 18,
-            10,  2, 59, 51, 43, 35, 27,
-            19, 11,  3, 60, 52, 44, 36,
+            57, 49, 41, 33, 25, 17, 9,
+            1, 58, 50, 42, 34, 26, 18,
+            10, 2, 59, 51, 43, 35, 27,
+            19, 11, 3, 60, 52, 44, 36,
             63, 55, 47, 39, 31, 23, 15,
-             7, 62, 54, 46, 38, 30, 22,
-            14,  6, 61, 53, 45, 37, 29,
-            21, 13,  5, 28, 20, 12,  4
+            7, 62, 54, 46, 38, 30, 22,
+            14, 6, 61, 53, 45, 37, 29,
+            21, 13, 5, 28, 20, 12, 4
         };
 
 
-        static public ulong Permute(ulong val, int[] changes)
+        public static ulong Permute(ulong val, int[] changes)
         {
             ulong result = 0;
-            const int size = sizeof (ulong) * 8;
+            const int size = sizeof (ulong)*8;
 
             for (int i = 0; i < changes.Length; i++)
             {
@@ -30,22 +30,32 @@ namespace DES
         }
 
         // Retrieve the left half of the 56 bit value
-        static public uint Left(ulong val)
+        public static ulong Left(ulong val)
         {
-            return (uint) ((val & 0xFFFFFFF000000000) >> 36);
+            return val & 0xFFFFFFF000000000;
         }
 
         // Retrieve the right half of the 56 bit value
-        static public uint Right(ulong val)
+        public static ulong Right(ulong val)
         {
-            return (uint)((val & 0x0000000FFFFFFF00) >> 8);
+            return (val << 28) & 0xFFFFFFF000000000;
         }
 
         // 56 bit left shift
-        static public uint LeftShift(uint val, int count)
+        public static ulong LeftShift(ulong val, int count)
         {
-            var msb = val & 0x08000000;
-            return (val << count) & 0x0FFFFFFF | msb >> 27;
+            var msb = val & 0x8000000000000000;
+            return (val << count) & 0xFFFFFFF000000000 | msb >> 27;
         }
+
+        public static int[] LeftShifts = {1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1};
+
+
+    }
+
+    public struct Pair
+    {
+        public uint Left;
+        public uint Right;
     }
 }
